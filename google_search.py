b95a400a27b8f3a0d6746f0e03b474023a080a03
Abhishek Upadhyay <47971436+abhishekupadhyay260@users.noreply.github.com>
2021-08-09 01:02:22 +0530

    Add files via upload

---
+++ google_search.py
@@ -0,0 +1,6 @@
+import pywhatkit # importing the module
+
+search_word = input("enter word or url to search \n")	#to take url input from user
+pywhatkit.search(search_word) # main program it will perform the Google search
+print("Searching...") #not must, i printed just to look cool
+
